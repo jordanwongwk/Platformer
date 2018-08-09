0a687e6fe1106ec20a07a40a3e7ef23fbdb70287
Jordan Wong <jordanwongwk@gmail.com>
2018-08-09 17:04:18 +0800

    Added in cinemaching. Adding climbing capability.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,22 +10,29 @@ public class Player : MonoBehaviour {
 
     Rigidbody2D myRigidBody;
     BoxCollider2D myFeetCollider;
+    CapsuleCollider2D myBodyCollider;
     Animator myAnimator;
+
+    float initialGravityScale;
 
     // Use this for initialization
     void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
         myFeetCollider = GetComponent<BoxCollider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
         myAnimator = GetComponent<Animator>();
+
+        initialGravityScale = myRigidBody.gravityScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Walk();
-        Jump();
+        Walking();
+        Jumping();
+        Climbing();
 	}
 
-    void Walk()
+    void Walking()
     {
         float horizontalMove = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerMove = new Vector2( horizontalMove * walkSpeed, myRigidBody.velocity.y);
@@ -44,7 +51,7 @@ public class Player : MonoBehaviour {
         }
     }
 
-    void Jump()
+    void Jumping()
     {
         if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Foreground"))) { return; }
 
@@ -52,6 +59,31 @@ public class Player : MonoBehaviour {
         {
             Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
             myRigidBody.velocity += jumpVelocity;
+        }
+    }
+
+    void Climbing()
+    {
+        if (!myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myAnimator.SetBool("isOnLadder", false);
+            myAnimator.SetBool("isClimbing", false);
+            myRigidBody.gravityScale = initialGravityScale;
+            return;
+        }
+        else if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myAnimator.SetBool("isOnLadder", true);
+            myRigidBody.gravityScale = 0f;
+        }
+
+        float verticalMove = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 playerMove = new Vector2(myRigidBody.velocity.x, verticalMove * walkSpeed);
+        myRigidBody.velocity = playerMove;
+
+        if (Mathf.Abs(verticalMove) > Mathf.Epsilon)
+        {
+            myAnimator.SetBool("isClimbing", true);
         }
     }
 }
