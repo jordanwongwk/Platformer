e41df1a72098ca82d73a3a35ecd512fd9b90886e
Jordan Wong <jordanwongwk@gmail.com>
2018-08-14 17:10:23 +0800

    End of Day Update: Revamp obstacle generator to make sure harder obstacles comes later in the game

--- Assets/ObstacleGenerator.cs
+++ Assets/ObstacleGenerator.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour {
 
+    [Header("General Settings")]
     [SerializeField] float navigateSpeed = 2.0f;
     [SerializeField] float generationCooldown = 2.0f;
-    [SerializeField] GameObject[] obstacleTiles;
+
+    [Header("Tier Threshold")]
+    [SerializeField] float changeToIntermediate = 250.0f;
+    [SerializeField] float changeToAdvanced = 750.0f;
+
+    [Header("Obstacles Array")]
+    [SerializeField] GameObject[] simpleObstacles;
+    [SerializeField] GameObject[] intermediateObstacles;
+    [SerializeField] GameObject[] advancedObstacles;
 
     int lastGeneratedObsInt = 0;
     bool isSpawningObstacles = false;
@@ -24,22 +33,39 @@ public class ObstacleGenerator : MonoBehaviour {
 
         if (!isSpawningObstacles && !obsGeneratorCollider.IsTouchingLayers(LayerMask.GetMask("ObstacleForeground")))
         {
-            // RNG for an integer
-            int generatedInt = Random.Range(0, obstacleTiles.Length);
-            
-            // No repeating tiles immediately
-            if (generatedInt == lastGeneratedObsInt)
-            {
-                return;
-            }
-
-            // Generating tile
-            lastGeneratedObsInt = generatedInt;
-            Debug.Log("Obstacle generated. Current int: " + lastGeneratedObsInt);
             StartCoroutine(SpawningCooldown());
-            var chosenObstacle = obstacleTiles[generatedInt];
-            Instantiate(chosenObstacle, transform.position, Quaternion.identity);
+
+            if (transform.position.y <= changeToIntermediate)
+            {
+                Debug.Log("simple");
+                SpawningObstacle(simpleObstacles);
+            }
+            else if (transform.position.y > changeToIntermediate && transform.position.y <= changeToAdvanced)
+            {
+                Debug.Log("intermediate");
+                SpawningObstacle(intermediateObstacles);
+            }
+            else if (transform.position.y > changeToAdvanced)
+            {
+                Debug.Log("advanced");
+                SpawningObstacle(advancedObstacles);
+            }
         }
+    }
+
+    void SpawningObstacle(GameObject[] obstacles)
+    {
+        // RNG for an integer
+        int generatedInt = Random.Range(0, obstacles.Length);
+
+        // No repeating tiles immediately
+        if (generatedInt == lastGeneratedObsInt) { return; }
+
+        // Generating tile
+        lastGeneratedObsInt = generatedInt;
+
+        var chosenObstacle = obstacles[generatedInt];
+        Instantiate(chosenObstacle, transform.position, Quaternion.identity);
     }
 
     IEnumerator SpawningCooldown()
