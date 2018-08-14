ae4fb1e6f23ab8b603874f2ff2933b0840124b7b
Jordan Wong <jordanwongwk@gmail.com>
2018-08-14 16:00:32 +0800

    Checkpoint Update 2: Added more obstacles, implemented climbing and spikes, fix a bug on player climbing ladders

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] Movement movementType;
+    [SerializeField] Vector2 movementMin;
+    [SerializeField] Vector2 movementMax;
+
+    public enum Movement { Horizontal, Vertical, Both }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (movementType)
+        {
+            case Movement.Horizontal:
+                if (transform.position.x < movementMin.x || transform.position.x >= movementMax.x)
+                {
+                    speed = -speed;
+                }
+                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+                break;
+
+            case Movement.Vertical:
+                if (transform.position.y < movementMin.y || transform.position.y >= movementMax.y)
+                {
+                    speed = -speed;
+                }
+                transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
+                break;
+
+            default:
+                Debug.Log("No log");
+                break;
+        }
+	}
+}
