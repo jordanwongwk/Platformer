fa228a4dbcd9901565d3dbf845e5cad2c3ed3e9e
Jordan Wong <jordanwongwk@gmail.com>
2018-08-15 17:01:31 +0800

    End of Day Update (15th Aug) : Successfully reconfigure and recode the platform movement [Previously: Insert initial and destination coordinates in inspector and move; Now: Make empty GameObject and child with a few destination points.] [Also have the freedom now to go through multiple points, going back-and-forth, and going through circular track]

---
+++ Assets/PlatformLine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformLine : MonoBehaviour {
+
+    [SerializeField] float lineWidth = 0.1f;
+
+    LineRenderer line;
+    List<GameObject> pointsList = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        line = GetComponent<LineRenderer>();
+
+        line.startWidth = lineWidth;
+        line.endWidth = lineWidth;
+        line.positionCount = transform.childCount;
+
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            line.SetPosition(i, transform.GetChild(i).transform.position);
+            pointsList.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public List<GameObject> GetPlatformPointsList()
+    {
+        return pointsList;
+    }
+}
--- Assets/_Script/MovingPlatform.cs
+++ Assets/_Script/MovingPlatform.cs
@@ -4,36 +4,91 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
 
-    public float speed = 5.0f;
-    [SerializeField] Movement movementType;
-    [SerializeField] Vector2 movementMin;
-    [SerializeField] Vector2 movementMax;
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float timeWaitAtPoint = 0.5f;
+    [SerializeField] GameObject designatedMoveLine;
+    [SerializeField] bool isMovingPositiveDirection = true;
+    [Tooltip("One-Way means it will go back and forth at proper manner; Circular means it will proceed as if there's a track.")]
+    [SerializeField] MovementType movePattern;
+    [Tooltip("Starting point")]
+    [SerializeField] int nextDestination = 0;
 
-    public enum Movement { Horizontal, Vertical, Both }
-	
-	// Update is called once per frame
-	void Update () {
-        switch (movementType)
+    public enum MovementType { OneWay, Circular }
+
+    List<GameObject> pointsList = new List<GameObject>();
+
+    float journeyCovered;
+    bool isResting = false;     // Ensure coroutine only run once
+
+    void Start()
+    {
+        pointsList = designatedMoveLine.GetComponent<PlatformLine>().GetPlatformPointsList();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!isResting)
         {
-            case Movement.Horizontal:
-                if (transform.position.x < movementMin.x || transform.position.x >= movementMax.x)
+            Move();
+            if (journeyCovered >= 1)
+            {
+                StartCoroutine(ChangeDestination());
+            }
+        }
+    }
+
+    private void Move()
+    {
+        float journey = speed * Time.deltaTime;
+        float totalDistance = Vector3.Distance(transform.position, pointsList[nextDestination].transform.position);
+        journeyCovered = journey / totalDistance;
+
+        transform.position = Vector3.Lerp(transform.position, pointsList[nextDestination].transform.position, journeyCovered);
+    }
+
+    IEnumerator ChangeDestination()
+    {
+        isResting = true;
+        yield return new WaitForSeconds (timeWaitAtPoint);
+        isResting = false;
+
+        switch (movePattern)
+        {
+            case MovementType.OneWay:
+                if (isMovingPositiveDirection) { nextDestination++; }
+                else if (!isMovingPositiveDirection) { nextDestination--; }
+
+                // The reason for using +2 / -2 is because if we +1 / -1, the point will return to the previous point
+                // This means it will remain at the point where the check is done. Thus, it will be backed by 2 units instead.
+                if (nextDestination >= pointsList.Count)
                 {
-                    speed = -speed;
+                    nextDestination -= 2;
+                    isMovingPositiveDirection = false;
                 }
-                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+                else if (nextDestination < 0)
+                {
+                    nextDestination += 2;
+                    isMovingPositiveDirection = true;
+                }
                 break;
 
-            case Movement.Vertical:
-                if (transform.position.y < movementMin.y || transform.position.y >= movementMax.y)
+            case MovementType.Circular:
+                if (isMovingPositiveDirection) { nextDestination++; }
+                else if (!isMovingPositiveDirection) { nextDestination--; }
+
+                if (nextDestination >= pointsList.Count)
                 {
-                    speed = -speed;
+                    nextDestination = 1;
                 }
-                transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
+                else if (nextDestination < 0)
+                {
+                    nextDestination = pointsList.Count - 2;
+                }
                 break;
 
             default:
-                Debug.Log("No log");
+                Debug.Log("Have you assigned a movement type?");
                 break;
         }
-	}
+    }
 }
