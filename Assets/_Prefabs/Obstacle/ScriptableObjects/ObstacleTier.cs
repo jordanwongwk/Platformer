a0e63b137fc29d254bb655dda96671550b9d4d8b
Jordan Wong <jordanwongwk@gmail.com>
2018-08-30 10:46:40 +0800

    Trying to refactor Obstacle Generator

---
+++ Assets/_Prefabs/Obstacle/ScriptableObjects/ObstacleTier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Platformer/Obstacle")]
+public class ObstacleTier : ScriptableObject {
+
+    [SerializeField] List<GameObject> obstacleList;
+
+    public GameObject GetObstacle (int number)
+    {
+        return obstacleList[number];
+    }
+}
